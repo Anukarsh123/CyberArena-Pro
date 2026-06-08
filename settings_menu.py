79077869562bd79bd3d4b9a41c0ac2e069518ff9
Anukarsh123 <anukarsh82@gmail.com>
2026-06-08 11:39:08 +0530

    Create settings_menu.py

---
+++ settings_menu.py
@@ -0,0 +1,14 @@
+class Settings:
+
+    def __init__(self):
+
+        self.music_volume = 50
+        self.sound_volume = 50
+
+    def set_music_volume(self, volume):
+
+        self.music_volume = volume
+
+    def set_sound_volume(self, volume):
+
+        self.sound_volume = volume
